a3c7b20751efc1ac9b8eae662e15a3651030d8a3
Alex Tesch <gatesch@gmail.com>
2017-08-18 03:51:36 +0000

    new commit from master

---
+++ ping-test.sh
@@ -0,0 +1,1 @@
+ansible mapr-all -m ping
---
+++ remove.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+dcos marathon app remove /comc1
+dcos marathon app remove /data1
+dcos marathon app remove /data2
+dcos marathon app remove /data3
+dcos marathon app remove /data4
