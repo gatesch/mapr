973c1dec9e075c9ca7ecc095ca2eb44524697087
Alex Tesch <gatesch@gmail.com>
2017-09-21 03:05:30 +0000

    take out health check

--- files/topology.sh
+++ files/topology.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-COMC1=`maprcli node list -columns id |grep comc2 |awk {'print $3'}`
-DATA1=`maprcli node list -columns id |grep data11 |awk {'print $3'}`
-DATA2=`maprcli node list -columns id |grep data12 |awk {'print $3'}`
-DATA3=`maprcli node list -columns id |grep data13 |awk {'print $3'}`
-DATA4=`maprcli node list -columns id |grep data14 |awk {'print $3'}`
+COMC1=`maprcli node list -columns id |grep comc1 |awk {'print $3'}`
+DATA1=`maprcli node list -columns id |grep data1 |awk {'print $3'}`
+DATA2=`maprcli node list -columns id |grep data2 |awk {'print $3'}`
+DATA3=`maprcli node list -columns id |grep data3 |awk {'print $3'}`
+DATA4=`maprcli node list -columns id |grep data4 |awk {'print $3'}`
 
 maprcli node move -serverids $COMC1 -topology /data/mkt/rack1
 maprcli node move -serverids $DATA1 -topology /data/dev/rack1
